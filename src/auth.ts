982e1893fcdc79647cc528e6e9f34cb2f241283b
billoria1234 <sunali123b@gmail.com>
2025-06-03 11:49:26 +0530

    your commit message1

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,24 @@
-// src/auth.ts
-import NextAuth from "next-auth";
-import { authOptions } from "./authOptions";
+import { AuthOptions, User } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = {
+          id: "1",
+          email: credentials?.email || "",
+        };
+
+        // Cast the object to User type
+        return user as User;
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+};
